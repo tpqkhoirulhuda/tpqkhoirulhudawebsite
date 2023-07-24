27ca6e075a62739783afa406e35add97488c945f
KoongHiap <yapkng@gmail.com>
2023-07-24 09:42:42 +0700

    script run npm and php artisan

---
+++ app/Console/Commands/ServeAndDev.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+
+class ServeAndDev extends Command
+{
+    protected $signature = 'start';
+    protected $description = 'Run npm start';
+
+    public function handle()
+    {
+        // Get the Laravel project root directory
+        $projectPath = base_path();
+
+        // Command to run npm start (You can modify this if needed)
+        $npmStartCommand = 'npm start';
+
+        // Execute npm start using proc_open for better process handling
+        $process = proc_open($npmStartCommand, [0 => STDIN, 1 => STDOUT, 2 => STDERR], $pipes, $projectPath);
+
+        // Wait for the process to finish
+        $status = proc_get_status($process);
+        while ($status['running']) {
+            usleep(100000); // Sleep for 100ms
+            $status = proc_get_status($process);
+        }
+
+        // Close the process
+        proc_close($process);
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,8 @@ class Kernel extends ConsoleKernel
     protected function commands()
     {
         $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__.'/Commands/ServeAndDev.php');
+
 
         require base_path('routes/console.php');
     }
