e55665d489ec8de126dea5d48da80991b30b038c
nehemiagueldi <nehemia.gueldi@student.umn.ac.id>
2023-07-27 17:18:52 +0700

    tambah modal untuk edit data dan delete data santri

--- resources/views/datasantri.blade.php
+++ resources/views/datasantri.blade.php
@@ -32,7 +32,6 @@
   <script>
     const listBox = document.querySelector('.box-list');
     const santri = @json($santri);
-    const editData = { name: "", jenis_kelamin: "", email: "", noTelp: "", alamat: "", tempat_lahir: "", tanggal_lahir: "" };
 
     const getSantri = (filteredSantri) => {
         listBox.innerHTML = filteredSantri.map((e) => {
@@ -41,17 +40,84 @@
                 <div class="collapse-title text-xl font-medium">
                     ${e.name}
                 </div>
-                
+
                 <div class="collapse-content"> 
-                    <p>Kelas: ${e.name}</p>
+                    <p>Kelas: ${e.kelas_id}</p>
                     <p>Jenis Kelamin: ${e.jenis_kelamin}</p>
                     <p>Email: ${e.email}</p>
                     <p>No Telp: ${e.noTelp}</p>
                     <p>Alamat: ${e.alamat}</p>
                     <p>Tempat, Tanggal Lahir: ${e.tempat_lahir}, ${e.tanggal_lahir}</p>
+                    <p>Nama Ibu: ${e.ibu}</p>
+                    <!-- BUTTON FOR OPEN MODAL -->
                     <div class="mt-2 space-x-2">
-                        <button class="btn btn-success btn-outline">Edit</button>
-                        <button class="btn btn-error btn-outline">Delete</button>
+                        <label for="edit" class="btn btn-success btn-outline">Edit</label>
+                        <label for="delete" class="btn btn-error btn-outline">Delete</label>
+                    </div>
+
+                    <!-- EDIT MODAL -->
+                    <input type="checkbox" id="edit" class="modal-toggle" />
+                    <div class="modal">
+                        <div class="modal-box">
+                            <div class="flex flex-col justify-center items-center p-4">
+                                <h3 class="font-bold text-lg">Edit Data Santri</h3>
+                                <form action="">
+                                    <label class="label">
+                                        <span class="label-text">Nama</span>
+                                    </label>
+                                    <input name="nama" type="text" value="${e.name}" class="input input-bordered w-full max-w-xs" />
+                                    <label class="label">
+                                        <span class="label-text">Kelas</span>
+                                    </label>
+                                    <input name="kelas" type="text" value="${e.kelas_id}" class="input input-bordered w-full max-w-xs" />
+                                    <label class="label">
+                                        <span class="label-text">Jenis Kelamin</span>
+                                    </label>
+                                    <input name="nama" type="text" value="${e.jenis_kelamin}" class="input input-bordered w-full max-w-xs" />
+                                    <label class="label">
+                                        <span class="label-text">Email</span>
+                                    </label>
+                                    <input name="email" type="email" value="${e.email}" class="input input-bordered w-full max-w-xs" />
+                                    <label class="label">
+                                        <span class="label-text">No Telp</span>
+                                    </label>
+                                    <input name="noTelp" type="text" value="${e.noTelp}" class="input input-bordered w-full max-w-xs" />
+                                    <label class="label">
+                                        <span class="label-text">Alamat</span>
+                                    </label>
+                                    <input name="alamat" type="text" value="${e.alamat}" class="input input-bordered w-full max-w-xs" />
+                                    <label class="label">
+                                        <span class="label-text">Tempat, Tanggal Lahir</span>
+                                    </label>
+                                    <input name="tempat_lahir" type="text" value="${e.tempat_lahir}" class="input input-bordered w-full max-w-xs" />
+                                    <input name="tanggal_lahir" type="date" value="${e.tanggal_lahir}" class="input input-bordered w-full max-w-xs" />
+                                    <label class="label">
+                                        <span class="label-text">Nama Ibu</span>
+                                    </label>
+                                    <input name="ibu" type="text" value="${e.ibu}" class="input input-bordered w-full max-w-xs" />
+                                </form>
+                            </div>
+                            
+                            <div class="modal-action">
+                                <label for="edit" class="btn btn-accent btn-outline">Save</label>
+                                <label for="edit" class="btn btn-info btn-outline">Cancel</label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- DELETE MODAL -->
+                    <input type="checkbox" id="delete" class="modal-toggle" />
+                    <div class="modal">
+                        <div class="modal-box">
+                            <div class="flex flex-col justify-center items-center p-4">
+                                <h3 class="font-bold text-lg">Delete Santri</h3>
+                                <p class="py-4">Apakah kamu yakin mau menghapus data ${e.name}</p>
+                                <div class="modal-action">
+                                    <label for="delete" class="btn btn-error btn-outline">Delete</label>
+                                    <label for="delete" class="btn btn-info btn-outline">Cancel</label>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
